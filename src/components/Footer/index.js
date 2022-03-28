c1039113534dac121eab2934b8d56d9e348cb470
ingramwd <Ingramwd01@gmail.com>
2022-03-28 00:29:03 -0400

    added links to socials in footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,6 +3,24 @@ import React from "react";
 const Footer = () => {
     return (
         <footer className="w-100 mt-auto  p-4">
+            <footer className="footer">
+                <div>
+                    <div className="d-flex justify-content-around">
+                        <a className="text-light text-decoration-none" href="https://github.com/Ingramwd">
+                            GitHub
+                        </a>
+                        <a className="text-light text-decoration-none"
+                            href="https://www.linkedin.com/in/william-ingram-26ab34224/"
+                        >
+                            LinkedIn
+                        </a>
+                        <a className="text-light text-decoration-none"
+                            href="https://twitter.com/William15999168">
+                            Twitter
+                        </a>
+                    </div>
+                </div>
+            </footer>
             <div className="container">&copy;2022 by William Ingram</div>
         </footer>
     );
